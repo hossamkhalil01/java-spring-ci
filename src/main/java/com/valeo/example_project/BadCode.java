d9b51355533eefa70848d1248c6e3c4da16ab70f
Hossam Khalil <hossam.khalil@valeo.com>
2022-12-04 11:19:32 +0200

    fixed bad smell function

--- src/main/java/com/valeo/example_project/BadCode.java
+++ src/main/java/com/valeo/example_project/BadCode.java
@@ -3,9 +3,23 @@ package com.valeo.example_project;
 
 
 public class BadCode {
-    public static void bad_function() {
-        while(true){
-		System.out.println("This is an infinite loop without condition");
+    public void bad_function() {
+	int x = -2;
+	System.out.println("This is an example of a code smell");
+        while(x < 5){
+		while (x<4){
+			if (x == 2){
+				System.out.println("X = 2");	
+			}else{
+				if (x==0){	
+					System.out.println("X = 0");	
+				}else{
+					System.out.println("X != 0");	
+				}
+			}
+		}
+		x += 1;
+
 	};
     }
 
