4a222d7c9a24027b8b9810cda99816af2b612c94
Hossam Khalil <hossam.khalil@valeo.com>
2022-12-04 11:14:41 +0200

    added exmaple of a code smell

---
+++ src/main/java/com/valeo/example_project/BadCode.java
@@ -0,0 +1,12 @@
+package com.valeo.example_project;
+
+
+
+public class BadCode {
+    public static void bad_function() {
+        while(true){
+		System.out.println("This is an infinite loop without condition");
+	};
+    }
+
+}
